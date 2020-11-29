33d863c6fa9192ccb7c416094bf8b08654eb1931
sosome2020 <letebelebrightsosome@gmail.com>
2020-11-29 23:02:24 +0900

    implemented login function

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
+    else
+      flash[:danger] = 'I failed to login'
+      render :new
+    end
+  end
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'Logged out'
+    redirect_to new_session_path
+  end
+end
