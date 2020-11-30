17feac213d3a6bddbbc1b6b164bc6dc2968966ad
sosome2020 <letebelebrightsosome@gmail.com>
2020-11-30 23:01:44 +0900

    Generating feeds cotroller, model & Views - image unplodder

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_29_122626) do
+ActiveRecord::Schema.define(version: 2020_11_30_125839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "feeds", force: :cascade do |t|
+    t.text "image"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
