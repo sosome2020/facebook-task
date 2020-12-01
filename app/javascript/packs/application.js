2e19a3a046c5a0821aabb8db7fe1f75ef426ea8e
sosome2020 <letebelebrightsosome@gmail.com>
2020-12-01 19:58:34 +0900

    added bootstrap and jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require ("jquery/dist/jquery.js")
+require ("bootstrap/dist/js/bootstrap.min")
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
