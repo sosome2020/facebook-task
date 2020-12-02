ed73ffa8a247ed68c10a26885cb7432ab0deb0c6
sosome2020 <letebelebrightsosome@gmail.com>
2020-12-02 14:09:23 +0900

    deleted the meaningless blank lines

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,28 +1,19 @@
 class FeedsController < ApplicationController
   before_action :set_feed, only: [:show, :edit, :update, :destroy]
-
-  # GET /feeds
-  # GET /feeds.json
   def index
     @feeds = Feed.all
   end
 
-  # GET /feeds/1
-  # GET /feeds/1.json
   def show
   end
 
-  # GET /feeds/new
   def new
     @feed = Feed.new
   end
 
-  # GET /feeds/1/edit
   def edit
   end
 
-  # POST /feeds
-  # POST /feeds.json
   def create
     @feed = Feed.new(feed_params)
     @feed.user_id=current_user.id
@@ -40,8 +31,7 @@ class FeedsController < ApplicationController
   def confirm
     @feed = Feed.new(feed_params)
   end
-  # PATCH/PUT /feeds/1
-  # PATCH/PUT /feeds/1.json
+
   def update
     respond_to do |format|
       if @feed.update(feed_params)
@@ -54,8 +44,6 @@ class FeedsController < ApplicationController
     end
   end
 
-  # DELETE /feeds/1
-  # DELETE /feeds/1.json
   def destroy
     @feed.destroy
     respond_to do |format|
@@ -65,12 +53,10 @@ class FeedsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
   def set_feed
     @feed = Feed.find(params[:id])
   end
 
-    # Only allow a list of trusted parameters through.
   def feed_params
     params.require(:feed).permit(:image, :content , :image_cache, :user_id)
   end
